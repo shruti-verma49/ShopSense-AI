c7c8b1baeb69eeeeb07bd4912ab8a8ed64b85118
Shruti Verma <shrutiv553@gmail.com>
2026-07-17 00:43:24 +0530

    Fixed Wishlist Context and Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { WishlistProvider } from "./context/WishlistContext";
+import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
@@ -7,18 +7,14 @@ import Wishlist from "./pages/Wishlist";
 
 function App() {
   return (
-    <WishlistProvider>
-      <BrowserRouter>
-        <div className="min-h-screen bg-gray-50">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/wishlist" element={<Wishlist />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </WishlistProvider>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+
+import { WishlistProvider } from "./context/WishlistContext";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <WishlistProvider>
+        <App />
+      </WishlistProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
